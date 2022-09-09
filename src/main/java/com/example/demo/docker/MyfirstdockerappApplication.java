7cea5185082743a2dd045ceb386e64e4ece7667a
arijeet.sahoo <arijeet.sahoo@replicon.com>
2022-09-09 12:24:03 -0600

    this is the 1st version

---
+++ src/main/java/com/example/demo/docker/MyfirstdockerappApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyfirstdockerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstdockerappApplication.class, args);
+	}
+
+	@GetMapping("/msg")
+	public String getMsg() {
+		return "welcome kapil G and very nice app :)";
+	}
+
+	@GetMapping("/msg2")
+	public String getMsg2() {
+		return "this is msg 2 for docker and hello...";
+	}
+	
+	
+	
+
+}
